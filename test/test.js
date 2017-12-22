b4a1cf3f6dcbc79e3ffa6d16208bf83058a4aa33
Daniel <eggerdnl@gmail.com>
2017-12-22 23:16:14 +0100

    Update tests

--- test/test.js
+++ test/test.js
@@ -92,5 +92,15 @@ describe('Monitor', function() {
 				done()
 			})
 		})
+		it('should return date and date_prognosed for a line', function(done) {
+			app.location.get('Westbahnhof, Innsbruck', (error, locations) => {
+				expect(locations).to.have.length.above(0)
+				app.monitor.get(locations[0], (error, monitor) => {
+					expect(monitor.departures[0].date).to.exist
+					expect(monitor.departures[0].date_planned).to.exist
+					done()
+				})
+			})
+		})
 	})
 })
